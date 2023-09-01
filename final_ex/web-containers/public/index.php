f83286cb0388f304c41a15993c2c75e85d9c7583
Dvoryashin <103428511+Dvoryashin@users.noreply.github.com>
2023-09-01 15:05:40 +0300

    Add files via upload

---
+++ final_ex/web-containers/public/index.php
@@ -0,0 +1,26 @@
+<?php
+$dbconn = pg_connect("host=localhost dbname=messenger user=admin password=root")
+or die('Could not connect: ' . pg_last_error());
+
+// Performing SQL query
+$query = 'SELECT * FROM test';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+// Printing results in HTML
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Free resultset
+pg_free_result($result);
+
+// Closing connection
+pg_close($dbconn);
+
+?>
